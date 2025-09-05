795c5d59619dab4d6ec2001e3456cbe82bba39d1
Niuller <150717121+Niuller@users.noreply.github.com>
2025-09-05 14:21:06 -0500

    Add files via upload

---
+++ Pythonproyects/Pythonproyects/AutoPy.bat
@@ -0,0 +1,12 @@
+mkdir PythonBasic2
+CD PythonBasic2 
+mkdir notebooks src
+cd src
+mkdir model, view, controller
+cd ..
+cd notebooks
+cd ..
+python -m venv .venv
+cd .venv/scripts/
+activate
+cd ..
